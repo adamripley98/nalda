e74226e7142316c58c99774fd4047e1ed972845c
Adam Ripley <aripley@wharton.upenn.edu>
2018-01-05 18:09:38 -1000

    cant figure out email

--- backend/routes.js
+++ backend/routes.js
@@ -1025,6 +1025,61 @@ module.exports = () => {
     });
   });
 
+  // Route to send an email to Nalda team
+  // TODO: implement
+  router.post('/contact', (req, res) => {
+    res.send({
+      success: false,
+      error: 'This feature has not been connected yet.'
+    });
+    // console.log('enters contact');
+    // const nodemailer = require('nodemailer');
+    //
+    // // Generate test SMTP service account from ethereal.email
+    // // Only needed if you don't have a real mail account for testing
+    //   // create reusable transporter object using the default SMTP transport
+    // // const transporter = nodemailer.createTransport({
+    // //   host: 'smtp.ethereal.email',
+    // //   port: 587,
+    // //   secure: false, // true for 465, false for other ports
+    // //   auth: {
+    // //     user: 'username', // generated ethereal user
+    // //     pass: 'password'  // generated ethereal password
+    // //   }
+    // // });
+    //
+    // const transporter = nodemailer.createTransport('smtps://user%40gmail.com:pass@smtp.gmail.com');
+    //
+    // // setup email data with unicode symbols
+    // const mailOptions = {
+    //   from: '"Fred Foo 👻" <foo@blurdybloop.com>', // sender address
+    //   to: 'adamripley@gmail.com', // list of receivers
+    //   subject: 'Hello ✔', // Subject line
+    //   text: 'Hello world?', // plain text body
+    //   html: '<b>Hello world?</b>' // html body
+    // };
+    //
+    // // send mail with defined transport object
+    // transporter.sendMail(mailOptions, (error, info) => {
+    //   if (error) {
+    //     // return console.log(error);
+    //     console.log('error sending', error);
+    //     res.send({
+    //       error: 'Error sending message' + error
+    //     });
+    //   } else {
+    //     console.log('wow it worked');
+    //     res.send({
+    //       data: 'lol it worked',
+    //     });
+    //   }
+    //   // Preview only available when sending through an Ethereal account
+    //   console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    //
+    //     // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@blurdybloop.com>
+    //     // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+    // });
+  });
   // Return the router for use throughout the application
   return router;
 };
