33efd4b9b2e659eab9e116a8ac13834ff49381ff
Adam Ripley <aripley@wharton.upenn.edu>
2018-01-23 23:29:10 -0500

    facebook oauth work

--- backend/passport/facebook.js
+++ backend/passport/facebook.js
@@ -6,15 +6,42 @@ const router = express.Router();
  * Backend file for logging in with facebook
  */
 module.exports = (passport) => {
-  router.get('/auth/facebook', passport.authenticate('facebook'));
+  router.get('/auth/facebook', passport.authenticate('facebook', { scope: ['email'], session: false}));
 
-  router.get('/auth/facebook/callback',
-    passport.authenticate('facebook', { failureRedirect: '/login' }),
-    (req, res) => {
-      // Successful authentication, redirect home.
-      res.redirect('/');
-    }
-  );
+  router.get('/auth/facebook/callback', (req, res, next) => {
+    passport.authenticate('facebook', {scope: ['email'], session: false}, (errPassport, user) => {
+      if (errPassport) {
+        console.log('it there');
+        res.send({
+          success: false,
+          error: errPassport,
+        });
+        // TODO redirect to error
+      } else {
+        // Built in passport login method
+        req.logIn(user, (loginErr) => {
+          // Error logging in
+          if (Object.keys(loginErr).length) {
+            console.log('it here');
+            res.send({
+              success: false,
+              error: loginErr,
+            });
+            // TODO redirect to error
+          } else {
+            // Finally, if there is no error, send back user
+            // res.send({
+            //   success: true,
+            //   error: '',
+            //   user,
+            // });
+            console.log('should log me in', req.session);
+            res.redirect('/');
+          }
+        });
+      }
+    })(req, res, next);
+  });
 
   return router;
 };
