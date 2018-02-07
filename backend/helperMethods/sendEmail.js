91f43d802cd9ff42bf36e511065c41967fe37e9c
ccabo1 <ccabo@seas.upenn.edu>
2018-02-06 23:17:15 -0500

    Styling email

--- backend/helperMethods/sendEmail.js
+++ backend/helperMethods/sendEmail.js
@@ -22,6 +22,12 @@ const sendWelcomeEmail = (user, cb) => {
     sgMail.setApiKey(process.env.SENDGRID_API_KEY);
     // Email addresses them by first name
     const displayName = user.name.split(' ').length > 1 ? user.name.split(' ')[0] : user.name;
+
+    // Set up the html
+    const html = (
+      `<div style=\"background:#efefef;background-color:#efefef;width:100%;padding:0px,16px,32px,16px;\" bg-color:\"#efefef\"><div style=\"background:#ffffff;background-color:#ffffff;width:100%;padding:16px;\" bg-color=\"#ffffff\">Hi ${displayName},<br/>Welcome to Nalda! Please verify your account at the following link:<br/><br/>http://localhost:3000/verify/${token}<br/></div></div>`
+    );
+
     // Create message
     // TODO get rid of hardcoded host name
     const msg = {
@@ -29,7 +35,8 @@ const sendWelcomeEmail = (user, cb) => {
       from: process.env.SENDGRID_EMAIL,
       subject: 'Welcome to Nalda, ' + displayName + '! Verify your account.',
       text: 'Hi ' + displayName + ',\n Welcome to Nalda! Please verify your account at the following link:\n\n' +
-      'http://' + 'localhost:3000' + '/verify/' + token + '\n\n'
+      'http://' + 'localhost:3000' + '/verify/' + token + '\n\n',
+      html: html,
     };
 
     // Send message
