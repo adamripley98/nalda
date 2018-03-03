872e9f3cf62d31b38fc34f83c00ff107f0451b71
ccabo1 <ccabo@seas.upenn.edu>
2018-03-03 11:47:35 -0500

    webpack updates

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -2,41 +2,43 @@ const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-    entry: [
-        './frontend/index'
+  entry: [
+    './frontend/index'
+  ],
+  module: {
+    rules: [
+      { test: /\.js?$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.s?css$/, loader: 'style-loader!css-loader!sass-loader' },
+      { test: /\.(png|jpg|gif)$/, use: [{ loader: 'file-loader', options: {}}] },
+      { test: /\.svg$/, use: [{loader: "babel-loader"}, {loader: "react-svg-loader", options: {jsx: true}}] },
     ],
-    module: {
-        rules: [
-            { test: /\.js?$/, loader: 'babel-loader', exclude: /node_modules/ },
-            { test: /\.s?css$/, loader: 'style-loader!css-loader!sass-loader' },
-        ],
-    },
-    resolve: {
-        extensions: ['.js', '.scss']
-    },
-    output: {
-        path: path.join(__dirname, '/public'),
-        publicPath: '/',
-        filename: 'bundle.js'
-    },
-    devtool: 'cheap-eval-source-map',
-    devServer: {
-        contentBase: './public',
-        hot: true
-    },
-    plugins: [
-        new webpack.optimize.OccurrenceOrderPlugin(),
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoEmitOnErrorsPlugin(),
-        new webpack.DefinePlugin({
-            "process.env": {
-                "NODE_ENV": JSON.stringify("production")
-            }
-        }),
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                warnings: false
-            }
-        })
-    ]
+  },
+  resolve: {
+    extensions: ['.js', '.scss']
+  },
+  output: {
+    path: path.join(__dirname, '/public'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devtool: 'cheap-eval-source-map',
+  devServer: {
+    contentBase: './public',
+    hot: true
+  },
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.DefinePlugin({
+      "process.env": {
+        "NODE_ENV": JSON.stringify("production")
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })
+  ]
 };
