5bd7b3a3c8768b5bf8e42b37fd630b70faa29142
ccabo1 <ccabo@seas.upenn.edu>
2018-06-07 17:41:48 -0700

    listing categories on homepage

--- frontend/components/home/ListingCategories.js
+++ frontend/components/home/ListingCategories.js
@@ -1,6 +1,5 @@
 // Import frameworks
 import React, {Component} from 'react';
-import PropTypes from 'prop-types';
 import {Link} from 'react-router-dom';
 
 // Import json
@@ -35,10 +34,5 @@ class ListingCategories extends Component {
     );
   }
 }
-
-// Prop validations
-ListingCategories.propTypes = {
-  content: PropTypes.array,
-};
 
 export default ListingCategories;
