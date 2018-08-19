82a11411491734710fb6e0b162a56cd7cd56e263
Adam Ripley <aripley@wharton.upenn.edu>
2018-08-19 14:38:31 -0700

    event added

--- frontend/containers/AppContainer.js
+++ frontend/containers/AppContainer.js
@@ -31,6 +31,7 @@ import Article from '../components/content/articles/Article';
 import Articles from '../components/content/articles/Articles';
 import Listing from '../components/content/listings/Listing';
 import Listings from '../components/content/listings/Listings';
+import Events from '../components/content/events/Events';
 import Video from '../components/content/videos/Video';
 import Videos from '../components/content/videos/Videos';
 import Categories from '../components/content/listings/Categories';
@@ -267,6 +268,7 @@ class AppContainer extends Component {
                 { /* Routes for events */ }
                 <Route exact path="/events/new" component={requireCurator(EventForm)} />
                 <Route exact path="/events/:id/edit" component={requireCurator(EditEventForm)} />
+                <Route exact path="/events" component={Events} />
 
                 { /* 404 if no other route was matched */ }
                 <Route exact path="/*" component={NotFoundSection}/>
