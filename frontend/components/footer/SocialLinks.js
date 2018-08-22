3d0f38ded297bd14d6a9b24cb6d13745ff679cda
Adam Ripley <aripley@wharton.upenn.edu>
2018-08-22 16:59:06 -0700

    social links

--- frontend/components/footer/SocialLinks.js
+++ frontend/components/footer/SocialLinks.js
@@ -5,22 +5,17 @@ export default () => (
   <div className="footer-social-links">
     <ul className="footer-social-icons">
       <li className="gf-social-item">
-        <Link to="http://facebook.com/edwardkim.co" target="_blank" className="gf-facebook">
+        <Link to="https://www.facebook.com/naldacampus/" target="_blank" className="gf-facebook">
           <i className="fa fa-facebook fa-lg" />
         </Link>
       </li>
       <li className="gf-social-item">
-        <Link to="http://twitter.com/imedwardkim" target="_blank" className="gf-twitter">
-          <i className="fa fa-twitter fa-lg" />
-        </Link>
-      </li>
-      <li className="gf-social-item">
-        <Link to="http://instagram.com/imedwardkim" target="_blank" className="gf-instagram">
+        <Link to="https://www.instagram.com/naldacampus/" target="_blank" className="gf-instagram">
           <i className="fa fa-instagram fa-lg" />
         </Link>
       </li>
       <li className="gf-social-item">
-        <Link to="http://instagram.com/imedwardkim" target="_blank" className="gf-youtube">
+        <Link to="https://www.instagram.com/naldacampus/" target="_blank" className="gf-youtube">
           <i className="fa fa-youtube-play fa-lg" />
         </Link>
       </li>
