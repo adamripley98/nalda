6d1cc210cd0463c6f46cbfdf240876436dcf9dc3
ccabo1 <ccabo@seas.upenn.edu>
2018-08-23 00:17:22 -0700

    New logo

--- frontend/components/footer/Logo.js
+++ frontend/components/footer/Logo.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import Logo from '../../assets/images/logos/logo-text.svg';
+
 export default () => (
   <div className="nalda-logo-container">
     <Link to="/">
-      <div className="nalda-logo" />
+      <Logo className="nalda-logo" />
     </Link>
   </div>
 );
